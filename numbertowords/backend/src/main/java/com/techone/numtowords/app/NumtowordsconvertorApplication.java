b355e8e716388602ac2c1ba5ab0b1a8a1a89c638
muralikm-dev <56745045+muralikm-dev@users.noreply.github.com>
2024-08-25 23:43:57 +1000

    Application file

---
+++ numbertowords/backend/src/main/java/com/techone/numtowords/app/NumtowordsconvertorApplication.java
@@ -0,0 +1,13 @@
+package com.techone.numtowords.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumtowordsconvertorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumtowordsconvertorApplication.class, args);
+	}
+
+}
